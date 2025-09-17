0366dc6b4dfedd1f996490fe6319c3b8acd54e69
YojouhanSeikatsu <rikusaito64@gmail.com>
2025-09-17 10:39:36 -0400

    added more things

--- rock/rock.js
+++ rock/rock.js
@@ -1,7 +1,59 @@
+let db;
+
 function newPost() {
     showPopUp("New Post", `
-        Title
+        Title: <input type="text" id="post-title" maxlength="200"><hr>
+        <textarea id="post-body" maxlength="1500" rows="10"></textarea>
     `);
+}
+
+function search() {
+    const sortby = document.getElementById("sortby").value;
+
+    if (sortby === "last-comment") {
+        db.ref("posts").orderByChild("last-comment").once("value", function(post_object) {
+            post_object.forEach(function(posts) {
+                var post = document.createElement("div");
+                post.setAttribute("class", "topic")
+
+                var title = document.createElement("div");
+                title.setAttribute("class", "title");
+                title.innerHTML = posts.val().title.slice(0, 100);
+
+                post.appendChild(title);
+                post.appendChild(document.createElement("hr"));
+
+                var body = document.createElement("div");
+                body.setAttribute("class", "body");
+                body.innerHTML = posts.val().body.slice(0, 500);
+
+                post.addEventListener("click", function() {
+                    showPopUp("Post", `
+                        <h2>${posts.val().title}</h2>
+                        <hr>
+                        ${posts.val().body}
+                        <hr>
+                    `);
+                })
+
+                post.appendChild(body);
+
+                document.getElementById("main").appendChild(post);
+            })
+        })
+    }
+}
+
+function setup() {
+    db.ref(`users/${getUsername()}`).once('value', function(object) {
+        if (!object.exists() || object.val().password !== getPassword() || (object.val().muted || false) || (object.val().trapped || false) || Date.now() - (object.val().sleep || 0) < 0) {
+            document.body.innerHTML = `<h1>Unknown error occurred. Either you are removed, muted, trapped, timed out, etc</h1><button onclick="window.location.replace('../pebble/pebble.html?ignore=true')">Pebble</button>`;
+            db.goOffline();
+            return;
+        }
+
+        search();
+    })
 }
 
 window.onload = function() {
