a11a2093ec102df11073aac6c4eb6901988dc8b5
YojouhanSeikatsu <rikusaito64@gmail.com>
2025-09-22 00:57:55 -0400

    included more warnings and updated the config

--- pebble/pebble.js
+++ pebble/pebble.js
@@ -1,10 +1,11 @@
 window.onload = function() {
     try {
         const firebaseConfig = {
-            apiKey: "AIzaSyDOAj76E00Rg8Qyc5DQndWXHtCy2umC6vA",
+            apiKey: "AIzaSyAsp44iKOav3dbHrViABHETRmAnRtQnVwA",
             authDomain: "chatter-97e8c.firebaseapp.com",
+            databaseURL: "https://chatter-97e8c-default-rtdb.firebaseio.com",
             projectId: "chatter-97e8c",
-            storageBucket: "chatter-97e8c.appspot.com",
+            storageBucket: "chatter-97e8c.firebasestorage.app",
             messagingSenderId: "281722915171",
             appId: "1:281722915171:web:3b136d8a0b79389f2f6b56",
             measurementId: "G-4CGJ1JFX58"
